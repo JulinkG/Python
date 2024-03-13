752f9eba9bf824f755d739bf7f8d66e19e051b00
JulinkG <julianmenachavez@gmail.com>
2024-03-12 18:04:12 -0600

    Carpeta de Juegos

--- Python_programas/Juegos/Adivina_el_número.py
+++ Python_programas/Juegos/Adivina_el_número.py
@@ -3,20 +3,24 @@ from random import randint
 intentos = 0
 numero_secreto = randint(1,100)
 nombre = input("Escriba su nombre: ")
+numero_estimado = 0
+print(f"{nombre}, tienes 8 intentos para adivinar el número en el que estoy pensando del 1 al 100 \n Si lo logras...")
 
-print(f"{nombre}, tienes 8 intentos para adivinar el número en el que estoy pensando\n Si lo logras...")
 
 while intentos < 8:
     numero_estimado = int(input("¿En qué número estoy pensando?"))
     intentos += 1
 
+    if numero_estimado not in range(1,101):
+        print("Tu número no se encuentra en el rango de números en los que he pensado...")
+        
     if numero_estimado < numero_secreto:
         print("El número es mayor")
     
-    if numero_estimado > numero_secreto:
+    elif numero_estimado > numero_secreto:
         print("El número es menor")
 
-    if numero_estimado == numero_secreto:
+    else:
         print(f"FELICIDADES {nombre}! Has adivinado en {intentos}, intentos")
         break
 
